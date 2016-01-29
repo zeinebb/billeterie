eb5586fa2caf5dd9d043afb901f3972e67b63774
zineb <zeineb.abdoune@gmail.com>
2016-01-29 16:20:01 +0100

    Add files

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/home", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function homeAction()
+    {
+        return $this->render('AppBundle:default:home.html.twig');
+    }
+    
+    
+     /**
+     * @Route("/post", name="post")
+     */
+    public function postAction()
+    {
+        return $this->render('AppBundle:page:post.html.twig');
+    }
+     
+     /**
+     * @Route("/posts", name="posts")
+     */
+     public function postsAction()
+    {
+        return $this->render('AppBundle:page:posts.html.twig');
+    }
+}
+
